dc7545346656e7035518614d887088061862e107
psat022 <prashant022satpute@gmail.com>
2025-03-13 16:04:51 +0530

    pushing1

---
+++ src/test/java/NewTest.java
@@ -0,0 +1,8 @@
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  //execution1
+  public void f() {
+  }
+}
---
+++ src/test/java/TestNg/SampleTest.java
@@ -0,0 +1,17 @@
+package TestNg;
+
+import org.testng.annotations.Test;
+
+public class SampleTest	 {
+
+	//INVOCATIONcOUNT: EXEC SAME METHOD WITH MULTIPLE TIMES
+	//INVOCATIONCOUNT=2 
+	@Test
+	public void createProduct()
+	{
+		
+		System.out.println("Product created");
+	}
+	
+
+}
